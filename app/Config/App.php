61dafa987290684dafa88ca611528847a2fabc58
Adriele Colossi <adriele.colossi4@gmail.com>
2022-05-15 00:20:06 -0300

    trying

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://adrielecolossi.github.io/gamerspace/';
+    public $baseURL = 'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------

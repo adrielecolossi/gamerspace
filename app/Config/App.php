cc7e69c55818682636eb55743d164b85239b28da
Adriele Colossi <adriele.colossi4@gmail.com>
2022-05-15 00:23:36 -0300

    trying

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://gamerspace.herokuapp.com/';
+    public $baseURL = 'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------

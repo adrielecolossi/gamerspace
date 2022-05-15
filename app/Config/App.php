0c7fa7ae18bf0bf84794a0a2b138d9b6eac47c97
Adriele Colossi <adriele.colossi4@gmail.com>
2022-05-15 00:22:02 -0300

    trying

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://adrielecolossi.github.io/gamerspace/';
+    public $baseURL = 'https://gamerspace.herokuapp.com/';
 
     /**
      * --------------------------------------------------------------------------
